7eaa1d68b7e32cfb4eb0abda481f7ab5b42d1fa8
Xavier Lepretre <xavier.lepretre@gmail.com>
2019-07-15 14:53:05 +0100

    Moved to async in tests.

--- test/hashLotto.js
+++ test/hashLotto.js
@@ -2,6 +2,8 @@ const HashLotto = artifacts.require("./HashLotto.sol");
 const expectedExceptionPromise = require("../utils/expectedExceptionPromise.js");
 const addEvmFunctions = require("../utils/evmFunctions.js");
 
+const { hexToUtf8, toBN, toWei, utf8ToHex } = web3.utils;
+
 contract('HashLotto', function(accounts) {
     
     assert.isAtLeast(accounts.length, 1);
@@ -11,35 +13,34 @@ contract('HashLotto', function(accounts) {
     const maxGas = 3000000;
     let isTestRPC, hashLotto;
 
-    const mineMany = function(times) {
+    const mineMany = async function(times) {
         if (times <= 0) {
-            return Promise.resolve();
+            return;
         }
-        return web3.evm.mine()
-            .then(() => mineMany(times - 1));
+        await web3.evm.mine();
+        await mineMany(times - 1);
     };
 
-    before("should identify TestRPC", function() {
-        return web3.eth.getNodeInfo()
-            .then(node => isTestRPC = node.indexOf("EthereumJS TestRPC") >= 0);
+    before("should identify TestRPC", async function() {
+        const node = await web3.eth.getNodeInfo();
+        isTestRPC = node.indexOf("EthereumJS TestRPC") >= 0;
     });
 
-    beforeEach("should deploy a new hashLotto", function() {
-        return HashLotto.new({ from: accounts[0], value: web3.utils.toWei("0.1") })
-            .then(created => hashLotto = created);
+    beforeEach("should deploy a new hashLotto", async function() {
+        hashLotto = await HashLotto.new({ from: accounts[0], value: toWei("0.1") });
     });
 
     it("should not be possible to markMyWord if not enough Weis", function() {
         return expectedExceptionPromise(
-            () => hashLotto.markMyWord(web3.utils.utf8ToHex("hello world"), 1, { from: accounts[0], gas: maxGas,
-                value: web3.utils.toBN(web3.utils.toWei("0.1")).sub(web3.utils.toBN(1)) }),
+            () => hashLotto.markMyWord(utf8ToHex("hello world"), 1, { from: accounts[0], gas: maxGas,
+                value: toBN(toWei("0.1")).sub(toBN(1)) }),
             maxGas);
     });
 
     it("should not be possible to markMyWord if ahead is 0", function() {
         return expectedExceptionPromise(
-            () => hashLotto.markMyWord(web3.utils.utf8ToHex("hello world"), 0, { from: accounts[0], gas: maxGas,
-                value: web3.utils.toWei("0.1") }),
+            () => hashLotto.markMyWord(utf8ToHex("hello world"), 0, { from: accounts[0], gas: maxGas,
+                value: toWei("0.1") }),
             maxGas);
     });
 
@@ -47,82 +48,72 @@ contract('HashLotto', function(accounts) {
 
         let blockMarked;
 
-        beforeEach("should markMyWord", function() {
-            return hashLotto.markMyWord(web3.utils.utf8ToHex("hello world"), 1, { from: accounts[0], value: web3.utils.toWei("0.1") })
-                .then(txObj => blockMarked = txObj.receipt.blockNumber);
+        beforeEach("should markMyWord", async function() {
+            blockMarked = (await hashLotto.markMyWord(utf8ToHex("hello world"), 1, { from: accounts[0], value: toWei("0.1") }))
+                .receipt.blockNumber;
         });
 
-        it("should have kept the balance", function() {
-            return web3.eth.getBalance(hashLotto.address)
-                .then(balance => assert.strictEqual(balance.toString(10), web3.utils.toWei("0.2")));
+        it("should have kept the balance", async function() {
+            const balance = await web3.eth.getBalance(hashLotto.address);
+            assert.strictEqual(balance, toWei("0.2"));
         });
 
-        it("should have saved the Ticket", function() {
-            return hashLotto.tickets(accounts[0])
-                .then(ticket => {
-                    assert.strictEqual(web3.utils.hexToUtf8(ticket[0]), "hello world");
-                    assert.strictEqual(ticket[1].toNumber(), blockMarked + 1);
-                });
+        it("should have saved the Ticket", async function() {
+            const ticket = await hashLotto.tickets(accounts[0]);
+            assert.strictEqual(hexToUtf8(ticket[0]), "hello world");
+            assert.strictEqual(ticket[1].toNumber(), blockMarked + 1);
         });
 
-        it("should overwrite saved Ticket", function() {
-            return hashLotto.markMyWord(web3.utils.utf8ToHex("not my world"), 2, { from: accounts[0], value: web3.utils.toWei("0.1") })
-                .then(txObj => blockMarked = txObj.receipt.blockNumber)
-                .then(() => hashLotto.tickets(accounts[0]))
-                .then(ticket => {
-                    assert.strictEqual(web3.utils.hexToUtf8(ticket[0]), "not my world");
-                    assert.strictEqual(ticket[1].toNumber(), blockMarked + 2);
-                });
+        it("should overwrite saved Ticket", async function() {
+            blockMarked = (await hashLotto.markMyWord(utf8ToHex("not my world"), 2, { from: accounts[0], value: toWei("0.1") }))
+                .receipt.blockNumber;
+            const ticket = await hashLotto.tickets(accounts[0]);
+            assert.strictEqual(hexToUtf8(ticket[0]), "not my world");
+            assert.strictEqual(ticket[1].toNumber(), blockMarked + 2);
         });
 
     });
 
     it("should not be possible to toldYouSo before the block number is big enough", function() {
         return expectedExceptionPromise(
-            () => hashLotto.toldYouSo(web3.utils.utf8ToHex("brag"), { from: accounts[0], gas: maxGas }),
+            () => hashLotto.toldYouSo(utf8ToHex("brag"), { from: accounts[0], gas: maxGas }),
             maxGas);
     });
 
-    it("should not be possible to toldYouSo after 256 blocks without markMyWord first", function() {
+    it("should not be possible to toldYouSo after 256 blocks without markMyWord first", async function() {
         if (!isTestRPC) this.skip("Needs TestRPC");
-        return web3.eth.getBlock("latest")
-            .then(block => {
-                if (block.number <= 256) {
-                    return mineMany(256 - block.number);
-                }
-            })
-            .then(() => expectedExceptionPromise(
-                () => hashLotto.toldYouSo(web3.utils.utf8ToHex("brag"), { from: accounts[0], gas: maxGas }),
-                maxGas));
+        const block = await web3.eth.getBlock("latest");
+        if (block.number <= 256) {
+            return await mineMany(256 - block.number);
+        }
+        await expectedExceptionPromise(
+            () => hashLotto.toldYouSo(utf8ToHex("brag"), { from: accounts[0], gas: maxGas }),
+            maxGas);
     });
 
-    it("should not be possible to toldYouSo with hash 0 after 255 blocks", function() {
+    it("should not be possible to toldYouSo with hash 0 after 255 blocks", async function() {
         if (!isTestRPC) this.skip("Needs TestRPC");
-        return hashLotto.markMyWord("0x0", 1, { from: accounts[0], value: web3.utils.toWei("0.1") })
-            .then(() => mineMany(256))
-            .then(() => expectedExceptionPromise(
-                () => hashLotto.toldYouSo(web3.utils.utf8ToHex("brag"), { from: accounts[0], gas: maxGas }),
-                maxGas));
+        await hashLotto.markMyWord("0x0", 1, { from: accounts[0], value: toWei("0.1") });
+        await mineMany(256);
+        await expectedExceptionPromise(
+            () => hashLotto.toldYouSo(utf8ToHex("brag"), { from: accounts[0], gas: maxGas }),
+            maxGas);
     });
 
-    it("should be possible to toldYouSo with hash 0 after 256 blocks", function() {
+    it("should be possible to toldYouSo with hash 0 after 256 blocks", async function() {
         if (!isTestRPC) this.skip("Needs TestRPC");
-        return hashLotto.markMyWord("0x0", 1, { from: accounts[0], value: web3.utils.toWei("0.1") })
-            .then(() => mineMany(257))
-            .then(() => hashLotto.toldYouSo(web3.utils.utf8ToHex("brag"), { from: accounts[0] }))
-            .then(txObj => {
-                assert.strictEqual(txObj.logs.length, 1);
-                const toldYouEvent = txObj.logs[0];
-                assert.strictEqual(toldYouEvent.args.who, accounts[0]);
-                assert.strictEqual(web3.utils.hexToUtf8(toldYouEvent.args.braggingRights), "brag");
-            })
-            .then(() => hashLotto.tickets(accounts[0]))
-            .then(ticket => {
-                    assert.strictEqual(web3.utils.hexToUtf8(ticket[0]), "");
-                    assert.strictEqual(ticket[1].toNumber(), 0);                
-            })
-            .then(() => web3.eth.getBalance(hashLotto.address))
-            .then(balance => assert.strictEqual(balance.toString(10), "0"));
+        await hashLotto.markMyWord("0x0", 1, { from: accounts[0], value: toWei("0.1") });
+        await mineMany(257);
+        const txObj = await hashLotto.toldYouSo(utf8ToHex("brag"), { from: accounts[0] });
+        assert.strictEqual(txObj.logs.length, 1);
+        const toldYouEvent = txObj.logs[0];
+        assert.strictEqual(toldYouEvent.args.who, accounts[0]);
+        assert.strictEqual(hexToUtf8(toldYouEvent.args.braggingRights), "brag");
+        const ticket = await hashLotto.tickets(accounts[0]);
+        assert.strictEqual(hexToUtf8(ticket[0]), "");
+        assert.strictEqual(ticket[1].toNumber(), 0);                
+        const balance = await web3.eth.getBalance(hashLotto.address);
+        assert.strictEqual(balance.toString(10), "0");
     });
 
 });
